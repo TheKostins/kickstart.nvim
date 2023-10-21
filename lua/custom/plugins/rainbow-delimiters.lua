e1e1605f46c9949ff18b8ecf62c721cc86ee2fa5
Konstantin <anisimovkosta@19gmail.com>
2023-10-21 16:18:49 +0300

    rainbow-delimiters

---
+++ lua/custom/plugins/rainbow-delimiters.lua
@@ -0,0 +1,24 @@
+return {
+  "hiphish/rainbow-delimiters.nvim",
+  config = function()
+    require 'rainbow-delimiters.setup'.setup {
+      highlight = {
+        'Red',
+        'Yellow',
+        'Blue',
+        'Orange',
+        'Green',
+        'Purple',
+      },
+      -- highlight = {
+      --   'RainbowDelimiterRed',
+      --   'RainbowDelimiterYellow',
+      --   'RainbowDelimiterBlue',
+      --   'RainbowDelimiterOrange',
+      --   'RainbowDelimiterGreen',
+      --   'RainbowDelimiterViolet',
+      --   'RainbowDelimiterCyan',
+      -- },
+    }
+  end,
+}
